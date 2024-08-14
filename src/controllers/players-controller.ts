f63080b40c22771357d902f96b46168a2f2c6029
Luan Ferreira <104697412+luanferreirab@users.noreply.github.com>
2024-08-14 15:50:52 -0300

    projeto champions - nodeJS

---
+++ src/controllers/players-controller.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express';
+import * as service from '../services/players-service';
+import { StatisticsModel } from '../models/statistics-model';
+
+export const getPlayer = async (req: Request, res: Response) => {
+    const HttpResponse = await service.getPlayerService();
+    
+    res.status(HttpResponse.statusCode).json(HttpResponse.body);
+};
+
+export const getPlayerById = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const HttpResponse = await service.getPlayerByIdService(id);
+    
+    res.status(HttpResponse.statusCode).json(HttpResponse.body);
+}
+
+export const postPlayer = async (req: Request, res: Response) => {
+    const bodyValue = req.body;
+    const HttpResponse = await service.createPlayerService(bodyValue);
+    
+    if(HttpResponse) {
+        res.status(HttpResponse.statusCode).json(HttpResponse.body);
+    }
+}
+
+export const deletePlayer = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const HttpResponse = await service.deletePlayerService(id);
+    
+    res.status(HttpResponse.statusCode).json(HttpResponse.body);
+}
+
+export const updatePlayer = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const bodyValue: StatisticsModel = req.body;
+    const HttpResponse = await service.updatePlayerService(id, bodyValue);
+    
+    res.status(HttpResponse.statusCode).json(HttpResponse.body);
+}
